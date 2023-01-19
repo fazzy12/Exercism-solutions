8dced71ab47a4abb989e30be1a68308ab6079372
fazzy12 <kaluifeanyi00@gmail.com>
2023-01-19 21:58:38 +0100

    first bash hello world

--- bash/hello_world.sh
+++ bash/hello_world.sh
@@ -1,15 +1,24 @@
-/*
-Instructions
-
-* The classical introductory exercise. Just say "Hello, World!".
-
-* "Hello, World!" is the traditional first program for beginning programming in a new language or environment.
-
-* The objectives are simple:
-*/
-
-//solution
-
 #!/usr/bin/env bash
 
 echo "Hello, World!"
+
+
+
+
+# Instructions
+
+# The classical introductory exercise. Just say "Hello, World!".
+
+# "Hello, World!" is the traditional first program for beginning programming in a new language or environment.
+
+#The objectives are simple:
+####
+
+
+
+
+
+
+
+
+#
